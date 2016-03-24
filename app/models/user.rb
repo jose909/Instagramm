3064585cf998011b1b9a17aab5ed1ebbdea22a4c
jose909 <rosaleayal19@hotmail.com>
2016-03-23 20:32:49 -0600

    Edit and New Page Styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-	has_many :pics	
+		
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :pics       
 
 end
